8c67b61443cdbc80b04a5be588b1e45a4771f0bb
Alessandro Perlo <aleperlo002@gmail.com>
2023-08-27 13:40:51 +0200

    Switched to firefox

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(visual-fill-column yasnippet corfu embark consult proof-general sly yasnippet-snippets yaml-mode which-key vterm vertico telega smartparens setup orderless minions markdown-mode marginalia magit ligature kind-icon haskell-mode embark-consult eglot ef-themes diredfl dired-subtree corfu-doc)))
+   '(mips-mode visual-fill-column yasnippet corfu embark consult proof-general sly yasnippet-snippets yaml-mode which-key vterm vertico telega smartparens setup orderless minions markdown-mode marginalia magit ligature kind-icon haskell-mode embark-consult eglot ef-themes diredfl dired-subtree corfu-doc)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
